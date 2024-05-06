09ee8dd8166f153cbfa1b7caebc4f65847c60ac6
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2024-05-06 19:21:46 +0300

    protobuf

--- packages/apps/service-a/src/index.ts
+++ packages/apps/service-a/src/index.ts
@@ -1,3 +1,7 @@
 import {exampleFunction} from 'lib-a'
-
+import root from 'proto'
+const buf = root.mynamespace.Msg.encode({field: "value"}).finish()
+const original = root.mynamespace.Msg.decode(buf)
+const orig2 = root.mynamespace.Msg.decode(buf)
 console.log("hi " + exampleFunction(1, 1))
+console.log(original.field,buf, orig2.field)
