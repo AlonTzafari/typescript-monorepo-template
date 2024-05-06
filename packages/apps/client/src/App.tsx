87b80c2381ae2342b5b35165ca2eeb98f0abd53e
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2024-05-06 20:16:54 +0300

    component library

--- packages/apps/client/src/App.tsx
+++ packages/apps/client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {MyButton, MButton} from 'ui-lib'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+        <MyButton label="myButton">BTN</MyButton>
+        <MButton/>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
